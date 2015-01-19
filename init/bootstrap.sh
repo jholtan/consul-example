7995005d3d970f10a29ffa493e99807a1a4d3f9d
Jarle Holtan <jarle.holtan@bekk.no>
2015-01-19 21:58:13 +0100

    Provision Consul and Consul Web UI

--- init/bootstrap.sh
+++ init/bootstrap.sh
@@ -1,4 +1,37 @@
 #!/bin/sh
 
-echo $1
-echo $2
+TYPE=$1
+WEBCLIENT=$2
+
+function err() {
+  echo $1
+  exit 1
+}
+
+function usage_and_exit() {
+  err "Usage: $0 [type]<server|client> [install web ui]<true|false>"
+}
+
+function install_consul() {
+  cd /tmp
+  curl -L -O https://dl.bintray.com/mitchellh/consul/0.4.1_linux_amd64.zip
+  cd /usr/local/bin/
+  unzip /tmp/0.4.1_linux_amd64.zip
+}
+
+function install_consul_web() {
+  cd /tmp
+  curl -L -O https://dl.bintray.com/mitchellh/consul/0.4.1_web_ui.zip
+  mkdir /opt/consul_web && cd /opt/consul_web
+  unzip /tmp/0.4.1_web_ui.zip
+}
+
+test -n "$TYPE" || usage_and_exit
+
+# Install dependencies
+yum install -y curl bind-utils
+
+test -e "/usr/local/bin/consul" || install_consul
+if [ $WEBCLIENT = "true" ]; then
+  test -d "/opt/consul_web" || install_consul_web
+fi
